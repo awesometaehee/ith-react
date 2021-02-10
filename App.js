fb5715ad1bae59989ad6a8215212b0253826b661
Taehee Im <imth1129@gmail.com>
2021-02-10 15:09:45 +0900

    react-native component

--- App.js
+++ App.js
@@ -2,14 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import HelloWorld from './components/HelloWorld';
+// import Counter from './components/Counter';
+// import LotsOfGreetings from './components/LotsOfGreetings';
+import SimpleList from './components/SimpleList';
+
+// App 컴포넌트 시작
 export default function App() {
+  // JSX 문법 -> UI 템플릿을 작성
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <HelloWorld></HelloWorld> */}
+      {/* <Counter></Counter> */}
+      {/* <LotsOfGreetings></LotsOfGreetings> */}
+      <SimpleList></SimpleList>
       <StatusBar style="auto" />
     </View>
   );
 }
+// App 컴포넌트 끝
 
 const styles = StyleSheet.create({
   container: {
